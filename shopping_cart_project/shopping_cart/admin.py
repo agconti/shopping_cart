be42184f9241aa23f612ac6663f6f5496680c7af
agconti <agc11d@gmail.com>
2013-08-31 17:02:14 -0400

    created basic customized admin interace

--- shopping_cart_project/shopping_cart/admin.py
+++ shopping_cart_project/shopping_cart/admin.py
@@ -1,8 +1,25 @@
 #admin.py
 from django.contrib import admin
-from shopping_cart.models import Merchant, Store, Item, Order
+from shopping_cart.models import Store, Item, Order
 
-admin.site.register(Merchant)
-admin.site.register(Store)
-admin.site.register(Item)
+class ItemInline(admin.TabularInline):
+	model = Item
+	extra = 3
+
+class StoreAdmin(admin.ModelAdmin):
+	list_display = ('name','bio')
+	fieldsets = [
+				( 'Store Information', {'fields': ['name','bio',], 'classes': ['collapse']}),
+	]
+	
+	exclude = ('owner',)
+	inlines = [ItemInline]
+    
+class ItemAdmin(admin.ModelAdmin):
+	list_display = ('name','price', 'quantity', 'description', 'store')
+	search_fields = ['name','store__name', 'price', 'quantity']
+	list_filter = ['store__name', 'date_added']
+
+admin.site.register(Store, StoreAdmin)
+admin.site.register(Item, ItemAdmin)
 admin.site.register(Order)
--- shopping_cart_project/shopping_cart/models.py
+++ shopping_cart_project/shopping_cart/models.py
@@ -4,30 +4,20 @@ from django.core.validators import MinValueValidator
 import datetime
 
 # Create your models here.
-class Merchant(models.Model):
-	name = models.CharField(max_length=200, unique=True)
-	user = models.OneToOneField(User)
-
-	def __unicode__(self):
-		'''
-		for human readable model representation 
-		'''
-		return "Merchant %s : Manager %s" %(self.name, self.user.username)
-
 class Store(models.Model):
 	name = models.CharField(max_length=200)
 	bio = models.TextField()
-	owner = models.OneToOneField(Merchant)
+	owner = models.OneToOneField(User)
 
 	def __unicode__(self):
 		'''
 		for human readable model representation 
 		'''
-		return "Store %s: by Merchant %s" %(self.name, self.owner.name)
+		return "Store %s: by Merchant %s" %(self.name, self.owner.username)
 
 class Item(models.Model):
 	name = models.CharField(max_length=200)
-	prices = models.DecimalField(max_digits=19, decimal_places=2)
+	price = models.DecimalField(max_digits=19, decimal_places=2)
 	description = models.TextField()
 	quantity = models.IntegerField(validators=[MinValueValidator(0)])
 	date_added = models.DateTimeField(auto_now_add=True)
@@ -42,7 +32,7 @@ class Item(models.Model):
 class Order(models.Model):
 	item = models.OneToOneField(Item)
 	quantity = quantity = models.IntegerField(validators=[MinValueValidator(0)])
-	user = models.OneToOneField(User)
+	buyer = models.OneToOneField(User)
 	date_ordered = models.DateTimeField(auto_now_add=True)
 
 	def __unicode__(self):
